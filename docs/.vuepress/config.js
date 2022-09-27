4f2867792e58312e0f88418cf6fe30e71036e431
Penfield Song <1371324574@qq.com>
2022-09-27 20:52:02 +0800

    feature: Create Project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "Node.js 文档",
+  description: "Node.js 学习文档及小案例展示",
+  base: '/Call-Me-By-My-Name/',
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "实例", link: "" },
+      {
+        text: "其他",
+        items: [
+          { text: "Github", link: "https://github.com/MuBaiYang123" },
+          { text: "博客", link: "https://mubaiyang123.github.io/" },
+        ],
+      },
+    ],
+  },
+};
